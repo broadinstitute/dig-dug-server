5e38815c6158d35412ad76144737a447396892d1
Preeti <singhpreeti236@gmail.com>
2019-07-10 14:24:47 -0400

    clean the start function

--- metadata.js
+++ metadata.js
@@ -1,19 +1,22 @@
 
 const request = require('request');
 var cachedMetadata;
-var cachedPhenotype;
-var cachedDataset;
+var cachedPhenotypes;
+var cachedDatasets;
 const axios = require('axios');
 const util = require('util')
 
-function getMetadata(host, port, mdv)
+function getMetadata(config)
 {
+  let host = config.kb.host;
+  let port = config.kb.port;
+  let expose = config.kb.expose;
+  let mdv = config.kb.mdv;
+  let baseurl = "http://" + host + ":" + port;
+
 //build a baseurl
-var kbPath = "http://" + host + ":" + port + "/dccservices/getMetadata?mdv=" + mdv;
-// request(path, function(error, response, body)
-// {
-//   cachedMetadata = JSON.parse(body);
-// })//get the data and store it in global variable
+  var kbPath = baseurl + "/dccservices/getMetadata?mdv=" + mdv;
+
 axios.get(kbPath)
 .then(function (response){
   cachedMetadata = response.data;
@@ -24,14 +27,20 @@ axios.get(kbPath)
 })
 }
 
+function getDatasets(){
+    
+  //traverse the cachedMetadata and get the datasets
+  return cachedMetadata.experiments;
+
+}
+
 function getPhenotypes(){
   //traverse the cachedMetadata and get the phenotypes
-  let phenotypes = cachedMetadata.experiments;
-  console.log("phenotypes are: " + phenotypes);
+
 }
 
 
 module.exports = {
   getMetadata: getMetadata,
   getPhenotypes: getPhenotypes
-};
+}
--- server.js
+++ server.js
@@ -32,8 +32,6 @@ const metadata = require('./metadata');
       });
   }
 
-  //http://ec2-34-229-106-174.compute-1.amazonaws.com:8090/dccservices/getMetadata?mdv=mdv140
-
   function route_kb_api_requests(config) {
       let host = config.kb.host;
       let port = config.kb.port;
@@ -50,18 +48,8 @@ const metadata = require('./metadata');
         //console.log(apiPath);
           if (data.method == 'POST') {
               app.post(`/${name}`, (req, res) => {
-                // urlExists(apiPath, function(err, exists){
-                //    if (exists){
-                //        req.pipe(request(apiPath)).pipe(res);
-                //    }
-                //    else{
-                //        res.status(404).send({error: "404: file not found"})
-                //    }
-                //  }
-
-                //  res.redirect(baseurl + data.path);
-              }); //app.post
-          } //if close
+              }); //close app.post
+          } //close if
           else if (data.method == 'GET') {
               app.get(`/${name}*`, (req, res) => {
                      console.log("query", req.query);
@@ -69,37 +57,16 @@ const metadata = require('./metadata');
                     req.pipe(request({qs:req.query, uri: apiPath, json: true})).pipe(res);
               }); //app.get
           } //else if
-
       }); //routeNames close
-
   }
-
-
-  // function linkProxy(fileName, url){
-  //   var fs = require('fs');
-  //   let fileStream = fs.createWriteStream(fileName);
-  //   request(url).pipe(fileStream);
-  // }
-
-
-
 
 function start(config) {
       route_github_static_content(config);
       route_kb_api_requests(config);
-
-          let host = config.kb.host;
-          let port = config.kb.port;
-          let mdv = config.kb.mdv;
-          //make the call to getMetadata and cache
-          metadata.getMetadata(host, port, mdv);
-          //console.log(promise);
-          //const promise2 = promise.then(cacheMetadata.getPhenotype());
-          app.listen(8090)
-
-
+      metadata.getMetadata(config);
+      app.listen(8090);
   }
 
   module.exports = {
     start: start
-  };
+  }
