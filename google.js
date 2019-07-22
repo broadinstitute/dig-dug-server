4a46f6186e8b949e1aa4d3be5e44f84a26239329
Quy <quy@broad.dev>
2019-07-22 11:05:43 -0400

    remove main/test functions

--- google.js
+++ google.js
@@ -10,22 +10,6 @@ const keys = require('./google.json');
 /**
  * Start by acquiring a pre-authenticated oAuth2 client.
  */
-async function main() {
-    const oAuth2Client = await getAuthenticatedClient();
-    // Make a simple request to the People API using our pre-authenticated client. The `request()` method
-    // takes an GaxiosOptions object.  Visit https://github.com/JustinBeckwith/gaxios.
-    const url = 'https://people.googleapis.com/v1/people/me?personFields=names';
-    const res = await oAuth2Client.request({url});
-    console.log(res.data);
-
-    // After acquiring an access_token, you may want to check on the audience, expiration,
-    // or original scopes requested.  You can do that with the `getTokenInfo` method.
-    const tokenInfo = await oAuth2Client.getTokenInfo(
-        oAuth2Client.credentials.access_token
-    );
-    console.log("token info email: >>> " + tokenInfo);
-    //console.log("hey: " + JSON.stringify(tokenInfo, null, 4));
-}
 
 const oAuth2Client = new OAuth2Client(
     keys.web.client_id,
@@ -39,7 +23,7 @@ const logInLink = function (req, res, next) {
         access_type: 'offline',
         scope: 'profile email openid',
     });
-    console.log("login link : " + authorizeUrl);
+    //console.log("login link : " + authorizeUrl);
     res.redirect(authorizeUrl);
 };
 
@@ -68,13 +52,13 @@ const getInfo = async function (code) {
     const r = await oAuth2Client.getToken(code);
     // Make sure to set the credentials on the OAuth2 client.
     oAuth2Client.setCredentials(r.tokens);
-    console.info('Tokens acquired.');
+    //console.info('Tokens acquired.');
 
     const tokenInfo = await oAuth2Client.getTokenInfo(
         oAuth2Client.credentials.access_token
     );
     //console.log("token info email: >>> " + tokenInfo.email);
-    console.log("info: " + JSON.stringify(tokenInfo, null, 4));
+    //console.log("info: " + JSON.stringify(tokenInfo, null, 4));
     //return tokenInfo;
 
     const url = 'https://people.googleapis.com/v1/people/me?personFields=names';
@@ -90,62 +74,6 @@ const getInfo = async function (code) {
 
    return user;
 };
-
-/**
- * Create a new OAuth2Client, and go through the OAuth2 content
- * workflow.  Return the full client to the callback.
- */
-function getAuthenticatedClient() {
-    return new Promise((resolve, reject) => {
-        // create an oAuth client to authorize the API call.  Secrets are kept in a `keys.json` file,
-        // which should be downloaded from the Google Developers Console.
-        const oAuth2Client = new OAuth2Client(
-            keys.web.client_id,
-            keys.web.client_secret,
-            keys.web.redirect_uris[0]
-        );
-
-        // Generate the url that will be used for the consent dialog.
-        const authorizeUrl = oAuth2Client.generateAuthUrl({
-            access_type: 'offline',
-            scope: 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email',
-        });
-        console.log("here: " + authorizeUrl);
-        // Open an http server to accept the oauth callback. In this simple example, the
-        // only request to our webserver is to /oauth2callback?code=<code>
-        const server = http
-            .createServer(async (req, res) => {
-                try {
-                    if (req.url.indexOf('/oauth2callback') > -1) {
-                        // acquire the code from the querystring, and close the web server.
-                        const qs = new url.URL(req.url, 'http://localhost:8090')
-                            .searchParams;
-                        const code = qs.get('code');
-                        console.log(`Code is ${code}`);
-                        res.end('Authentication successful! Please return to the console.');
-                        server.destroy();
-
-                        // Now that we have the code, use that to acquire tokens.
-                        const r = await oAuth2Client.getToken(code);
-                        // Make sure to set the credentials on the OAuth2 client.
-                        oAuth2Client.setCredentials(r.tokens);
-                        console.info('Tokens acquired.');
-                        resolve(oAuth2Client);
-                    }
-                } catch (e) {
-                    reject(e);
-                }
-            })
-            .listen(3000, () => {
-                // open the browser to the authorize url to start the workflow
-                //console.log(authorizeUrl);
-                opn(authorizeUrl, {wait: false}).then(cp => cp.unref());
-            });
-        destroyer(server);
-    });
-}
-
-//main().catch(console.error);
 
 module.exports = {
     logInLink: logInLink,
