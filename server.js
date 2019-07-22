3b14e6768a67af73628d214932a91fb13f5e7c49
Quy <quy@broad.dev>
2019-07-22 13:24:49 -0400

    remove conflict comments

--- server.js
+++ server.js
@@ -19,15 +19,14 @@ app.use(cookieParser());
 
 /* GET home page. */
 app.get('/', function(req, res, next) {
-    let name = req.cookies.name;
-    res.render('index', { title: 'Dig Dug Portal', name: name });
+	let name = req.cookies.name;
+	res.render('index', { title: 'Dig Dug Portal', name: name });
 });
 
 //Google authentication
 app.get('/login', google.logInLink);
 app.get('/oauth2callback', google.oauth2callback);
 
-<<<<<<< HEAD
 //Get datasets
 app.get("/getDatasets", (req, res) => {
 	let dataset = metadata.getDatasets();
@@ -48,163 +47,96 @@ function route_shared_static_content(config) {
 }
 
 function route_kb_api_requests(config) {
-      let host = config.kb.host;
-      let port = config.kb.port;
-      let expose = config.kb.expose;
-      let mdv = config.kb.mdv;
-      //REMEMBER TO ENABLE SSL BEFORE GOING LIVE
-      let baseurl = "http://" + host + ":" + port;
-      let routeNames = Object.keys(expose); //what entry names are exposed?
-      routeNames.forEach((name) => {
-        let data =  expose[name];
-        //build the api link
-        let apiPath = baseurl + data.urlpath;
-          if (data.method == 'POST') {
-              app.post(`/${name}`, (req, res) => {
-              });
-          }
-          else if (data.method == 'GET') {
-              app.get(`/${name}*`, (req, res) => {
-                     console.log("query", req.query);
-                     //REMEMBER - check url exists
-                    req.pipe(request({qs:req.query, uri: apiPath, json: true})).pipe(res);
-              });
-          }
-      });
-  }
+	let host = config.kb.host;
+	let port = config.kb.port;
+	let expose = config.kb.expose;
+	let mdv = config.kb.mdv;
+	//REMEMBER TO ENABLE SSL BEFORE GOING LIVE
+	let baseurl = "http://" + host + ":" + port;
+	let routeNames = Object.keys(expose); //what entry names are exposed?
+	routeNames.forEach((name) => {
+		let data =  expose[name];
+		//build the api link
+		let apiPath = baseurl + data.urlpath;
+		if (data.method == 'POST') {
+			app.post(`/${name}`, (req, res) => {
+			});
+		}
+		else if (data.method == 'GET') {
+			app.get(`/${name}*`, (req, res) => {
+				console.log("query", req.query);
+				//REMEMBER - check url exists
+				req.pipe(request({qs:req.query, uri: apiPath, json: true})).pipe(res);
+			});
+		}
+	});
+}
 
 function route_github_static_content(config, www) {
-    const resourcePath = www ? www : config.content.www; //set www to override if passed in
-    const checkPath = new URL(resourcePath);
+	const resourcePath = www ? www : config.content.www; //set www to override if passed in
+	const checkPath = new URL(resourcePath);
 
-    //static resources are served local instead of github
-    if(checkPath.protocol == 'file:') {
-        //serve static files from location in the config
-        app.use('/www', express.static(url.fileURLToPath(resourcePath)) );
+	//static resources are served local instead of github
+	if(checkPath.protocol == 'file:') {
+		//serve static files from location in the config
+		app.use('/www', express.static(url.fileURLToPath(resourcePath)) );
 
-        //serve static files from location relative to where server is run, i.e. public folder
-        //app.use('/www', express.static(path.join(__dirname, 'www')) );
-    } else {
-        app.get('/www/:filePath*', function (req, res) {
-            let filePath = resourcePath + "/" + req.params.filePath;
-            //console.log("file: " + filePath);
+		//serve static files from location relative to where server is run, i.e. public folder
+		//app.use('/www', express.static(path.join(__dirname, 'www')) );
+	} else {
+		app.get('/www/:filePath*', function (req, res) {
+			let filePath = resourcePath + "/" + req.params.filePath;
+			//console.log("file: " + filePath);
 
-            //check for error if file doesn't exist
-            urlExists(filePath, function(err, exists){
-                if (exists){
-                    req.pipe(request(filePath)).pipe(res)
-                }
-                else{
-                    res.status(404).send({error: "404: file not found"})
-                }
-            })
-        });
-    }
+			//check for error if file doesn't exist
+			urlExists(filePath, function(err, exists){
+				if (exists){
+					req.pipe(request(filePath)).pipe(res)
+				}
+				else{
+					res.status(404).send({error: "404: file not found"})
+				}
+			})
+		});
+	}
 }
 
 //function not used yet
 function route_shared_static_content(config) {
-    let path = config.content.shared;
-    app.get('/shared/:path*', (req, res) => {
-        res.redirect(`${path}/${req.path}`);
-    });
+	let path = config.content.shared;
+	app.get('/shared/:path*', (req, res) => {
+		res.redirect(`${path}/${req.path}`);
+	});
 }
 
 function route_kb_api_requests(config) {
-    let host = config.kb.host;
-    let port = config.kb.port;
-    let expose = config.kb.expose;
-    let mdv = config.kb.mdv;
-    //REMEMBER TO ENABLE SSL BEFORE GOING LIVE
-    let baseurl = "http://" + host + ":" + port;
-    let routeNames = Object.keys(expose); //what entry names are exposed?
-    routeNames.forEach((name) => {
-        let data =  expose[name];
-        //build the api link
-        let apiPath = baseurl + data.urlpath;
-        if (data.method == 'POST') {
-            app.post(`/${name}`, (req, res) => {
-            });
-        }
-=======
-function route_github_static_content(config, www) {
-    const resourcePath = www ? www : config.content.www; //set www to override if passed in
-    const checkPath = new URL(resourcePath);
+	let host = config.kb.host;
+	let port = config.kb.port;
+	let expose = config.kb.expose;
+	let mdv = config.kb.mdv;
+	//REMEMBER TO ENABLE SSL BEFORE GOING LIVE
+	let baseurl = "http://" + host + ":" + port;
+	let routeNames = Object.keys(expose); //what entry names are exposed?
+	routeNames.forEach((name) => {
+		let data =  expose[name];
+		//  console.log(name);
+		//build the api link
+		let apiPath = baseurl + data.urlpath;
+		//console.log(apiPath);
+		if (data.method == 'POST') {
+			app.post(`/${name}`, (req, res) => {
+			});
+		}
+	else if (data.method == 'GET') {
+		app.get(`/${name}*`, (req, res) => {
+			console.log("query", req.query);
+			//REMEMBER - check url exists
+			req.pipe(request({qs:req.query, uri: apiPath, json: true})).pipe(res);
 
-    //static resources are served local instead of github
-    if(checkPath.protocol == 'file:') {
-        //serve static files from location in the config
-        app.use('/www', express.static(url.fileURLToPath(resourcePath)) );
+		});
+		}
 
-        //serve static files from location relative to where server is run, i.e. public folder
-        //app.use('/www', express.static(path.join(__dirname, 'www')) );
-    } else {
-        app.get('/www/:filePath*', function (req, res) {
-            let filePath = resourcePath + "/" + req.params.filePath;
-            //console.log("file: " + filePath);
-
-            //check for error if file doesn't exist
-            urlExists(filePath, function(err, exists){
-                if (exists){
-                    req.pipe(request(filePath)).pipe(res)
-                }
-                else{
-                    res.status(404).send({error: "404: file not found"})
-                }
-            })
-        });
-    }
-}
-
-//function not used yet
-function route_shared_static_content(config) {
-    let path = config.content.shared;
-    app.get('/shared/:path*', (req, res) => {
-        res.redirect(`${path}/${req.path}`);
-    });
-}
-
-//http://ec2-34-229-106-174.compute-1.amazonaws.com:8090/dccservices/getMetadata?mdv=mdv140
-
-function route_kb_api_requests(config) {
-    let host = config.kb.host;
-    let port = config.kb.port;
-    let expose = config.kb.expose;
-    let mdv = config.kb.mdv;
-    //REMEMBER TO ENABLE SSL BEFORE GOING LIVE
-    let baseurl = "http://" + host + ":" + port;
-    let routeNames = Object.keys(expose); //what entry names are exposed?
-    routeNames.forEach((name) => {
-        let data =  expose[name];
-        //  console.log(name);
-        //build the api link
-        let apiPath = baseurl + data.urlpath;
-        //console.log(apiPath);
-        if (data.method == 'POST') {
-            app.post(`/${name}`, (req, res) => {
-                // urlExists(apiPath, function(err, exists){
-                //    if (exists){
-                //        req.pipe(request(apiPath)).pipe(res);
-                //    }
-                //    else{
-                //        res.status(404).send({error: "404: file not found"})
-                //    }
-                //  }
-
-                //  res.redirect(baseurl + data.path);
-            }); //app.post
-        } //if close
->>>>>>> a180d761aba036c6f271e67893e62e25161cb37d
-        else if (data.method == 'GET') {
-            app.get(`/${name}*`, (req, res) => {
-                console.log("query", req.query);
-                //REMEMBER - check url exists
-                req.pipe(request({qs:req.query, uri: apiPath, json: true})).pipe(res);
-<<<<<<< HEAD
-            });
-        }
-
-    });
+	});
 
 }
 
@@ -215,46 +147,8 @@ function start(config) {
 	var promise2 = promise1.then(function() {
 		app.listen(8090);
 	});
-=======
-            }); //app.get
-        } //else if
-
-    }); //routeNames close
-
-}
-
-// function linkProxy(fileName, url){
-//   var fs = require('fs');
-//   let fileStream = fs.createWriteStream(fileName);
-//   request(url).pipe(fileStream);
-// }
-
-function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-}
-
-function start(config, www) {
-    route_github_static_content(config, www);
-    route_kb_api_requests(config);
-
-    let host = config.kb.host;
-    let port = config.kb.port;
-    let mdv = config.kb.mdv;
-    //make the call to getMetadata and cache
-    metadata.getMetadata(host, port, mdv);
-    //console.log(promise);
-    //const promise2 = promise.then(cacheMetadata.getPhenotype());
-    //metadata.getPhenotypes();
-
-    app.listen(8090);
-
->>>>>>> a180d761aba036c6f271e67893e62e25161cb37d
 }
 
 module.exports = {
-    start: start
+	start: start
 };
