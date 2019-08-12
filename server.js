fd7de486578810355cd9ce51db49979ee9a8c471
Quy <quy@broad.dev>
2019-08-12 13:02:44 -0400

    add endpoint for getdataset with phenotype input

--- server.js
+++ server.js
@@ -1,135 +1,148 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const path = require('path');
-const url = require('url');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+const url = require("url");
 const app = express();
-const util = require('util');
-const yaml = require('js-yaml');
-const fs = require('fs');
-const urlExists = require('url-exists');
-const request = require('request');
-const log4js = require('log4js');
+const util = require("util");
+const yaml = require("js-yaml");
+const fs = require("fs");
+const urlExists = require("url-exists");
+const request = require("request");
+const log4js = require("log4js");
 
-const metadata = require('./metadata');
-const google = require('./google');
+const metadata = require("./metadata");
+const google = require("./google");
 
 //configure logger
 log4js.configure({
-    appenders: {
-      out: {
-        type: 'stdout',
-        layout: {
-          type: 'pattern', pattern: '%[[%p] %f{1}:%l >>%] %m'
-        }
-      }
-    },
-    categories: {
-      default: { appenders: ['out'], level: 'all', enableCallStack: true }
-    }
+	appenders: {
+		out: {
+			type: "stdout",
+			layout: {
+				type: "pattern",
+				pattern: "%[[%p] %f{1}:%l >>%] %m"
+			}
+		}
+	},
+	categories: {
+		default: { appenders: ["out"], level: "all", enableCallStack: true }
+	}
 });
 const logger = log4js.getLogger();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
 app.use(cookieParser());
 
 /* GET home page. */
-app.get('/', function(req, res, next) {
-    let name = req.cookies.name;
-    res.render('index', { title: 'Dig Dug Portal', name: name });
+app.get("/", function(req, res, next) {
+	let name = req.cookies.name;
+	res.render("index", { title: "Dig Dug Portal", name: name });
 });
 
 //Google authentication
-app.get('/login', google.logInLink);
-app.get('/oauth2callback', google.oauth2callback);
+app.get("/login", google.logInLink);
+app.get("/oauth2callback", google.oauth2callback);
 
 //Get datasets
 app.get("/getDatasets", (req, res) => {
-    let dataset = metadata.getDatasets();
-    res.json(dataset);
+	let datasets = metadata.getDatasets();
+	res.json(datasets);
 });
+
+//get datasets w/ given phenotype
+app.get("/getDatasets/:phenotype", (req, res) => {
+	let datasets = metadata.getDatasets(req.params.phenotype);
+	res.json(datasets);
+});
+
+//get data for given dataset
+app.post("/getData/:dataset/:phenotype", (req, res) => {
+	let variants = metadata.getData(req.params.dataset, req.params.phenotype);
+	res.json(variants);
+});
+
 //Get phenotypes
 app.get("/getPhenotypes", (req, res) => {
-    let phenotypes = metadata.getPhenotypes();
-    res.json(phenotypes);
+	let phenotypes = metadata.getPhenotypes();
+	res.json(phenotypes);
 });
 
 //function not used yet
 function route_shared_static_content(config) {
-    let path = config.content.shared;
-    app.get('/shared/:path*', (req, res) => {
-        res.redirect(`${path}/${req.path}`);
-    });
+	let path = config.content.shared;
+	app.get("/shared/:path*", (req, res) => {
+		res.redirect(`${path}/${req.path}`);
+	});
 }
 
 function route_kb_api_requests(config) {
-    let host = config.kb.host;
-    let port = config.kb.port;
-    let expose = config.kb.expose;
-    let mdv = config.kb.mdv;
-    //REMEMBER TO ENABLE SSL BEFORE GOING LIVE
-    let baseurl = "http://" + host + ":" + port;
-    let routeNames = Object.keys(expose); //what entry names are exposed?
-    routeNames.forEach((name) => {
-        let data =  expose[name];
-        //build the api link
-        let apiPath = baseurl + data.urlpath;
-        if (data.method == 'POST') {
-            app.post(`/${name}`, (req, res) => {
-            });
-        }
-        else if (data.method == 'GET') {
-            app.get(`/${name}*`, (req, res) => {
-                console.log("query", req.query);
-                //REMEMBER - check url exists
-                req.pipe(request({qs:req.query, uri: apiPath, json: true})).pipe(res);
-            });
-        }
-    });
+	let host = config.kb.host;
+	let port = config.kb.port;
+	let expose = config.kb.expose;
+	let mdv = config.kb.mdv;
+	//REMEMBER TO ENABLE SSL BEFORE GOING LIVE
+	let baseurl = "http://" + host + ":" + port;
+	let routeNames = Object.keys(expose); //what entry names are exposed?
+	routeNames.forEach(name => {
+		let data = expose[name];
+		//build the api link
+		let apiPath = baseurl + data.urlpath;
+		if (data.method == "POST") {
+			app.post(`/${name}`, (req, res) => {});
+		} else if (data.method == "GET") {
+			app.get(`/${name}*`, (req, res) => {
+				console.log("query", req.query);
+				//REMEMBER - check url exists
+				req.pipe(
+					request({ qs: req.query, uri: apiPath, json: true })
+				).pipe(res);
+			});
+		}
+	});
 }
 
 function route_github_static_content(config, www) {
-    const resourcePath = www ? www : config.content.www; //set www to override if passed in
-    const checkPath = new URL(resourcePath);
+	const resourcePath = www ? www : config.content.www; //set www to override if passed in
+	const checkPath = new URL(resourcePath);
 
-    //static resources are served local instead of github
-    if(checkPath.protocol == 'file:') {
-        //serve static files from location in the config
-        app.use('/www', express.static(url.fileURLToPath(resourcePath)) );
+	//static resources are served local instead of github
+	if (checkPath.protocol == "file:") {
+		//serve static files from location in the config
+		app.use("/www", express.static(url.fileURLToPath(resourcePath)));
 
-        //serve static files from location relative to where server is run, i.e. public folder
-        //app.use('/www', express.static(path.join(__dirname, 'www')) );
-    } else {
-        app.get('/www/:filePath*', function (req, res) {
-            let filePath = resourcePath + "/" + req.params.filePath;
-            //console.log("file: " + filePath);
+		//serve static files from location relative to where server is run, i.e. public folder
+		//app.use('/www', express.static(path.join(__dirname, 'www')) );
+	} else {
+		app.get("/www/:filePath*", function(req, res) {
+			let filePath = resourcePath + "/" + req.params.filePath;
+			//console.log("file: " + filePath);
 
-            //check for error if file doesn't exist
-            urlExists(filePath, function(err, exists){
-                if (exists){
-                    req.pipe(request(filePath)).pipe(res)
-                }
-                else{
-                    res.status(404).send({error: "404: file not found"})
-                }
-            })
-        });
-    }
+			//check for error if file doesn't exist
+			urlExists(filePath, function(err, exists) {
+				if (exists) {
+					req.pipe(request(filePath)).pipe(res);
+				} else {
+					res.status(404).send({ error: "404: file not found" });
+				}
+			});
+		});
+	}
 }
 
 function start(config) {
-    route_github_static_content(config);
-    route_kb_api_requests(config);
-    google.useConfig(config);
-    logger.info("Getting Metadata, please wait ... ");
-    var promise1 = metadata.getMetadata(config);
-    var promise2 = promise1.then(function() {
-        app.listen(8090, () => logger.info("Server started!"));
-    });
+	route_github_static_content(config);
+	route_kb_api_requests(config);
+	google.useConfig(config);
+	logger.info("Getting Metadata, please wait ... ");
+	var promise1 = metadata.getMetadata(config);
+	var promise2 = promise1.then(function() {
+		app.listen(8090, () => logger.info("Server started!"));
+	});
 }
 
 module.exports = {
-    start: start
+	start: start
 };
