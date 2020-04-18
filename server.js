edae68999c417ce634abf9bf86c172e7a85ba379
Richard Bruskiewich <richard@starinformatics.com>
2020-04-17 23:42:42 -0700

    Another session capture iteration

--- server.js
+++ server.js
@@ -72,6 +72,47 @@ function validateConfig(config) {
 
     return valid;
 }
+
+// middleware that creates an anonymous session when no registered user session is seen
+function getOrCreateSession() {
+    return function (req, res, next) {
+
+        let fullClientIp = requestIp.getClientIp(req);
+        logger.debug('fullClientIp:', fullClientIp);
+        let clientIpPart = fullClientIp.split(':');
+        logger.debug('clientIpPart:', clientIpPart);
+        let clientIp = clientIpPart[clientIpPart.length - 1]
+        logger.debug('Client IP:', clientIp);
+
+        logger.debug("Request Headers:\n", JSON.stringify(req.headers));
+        logger.debug('Request Type:', req.method);
+
+        let session = false;
+        if (req.cookies) {
+            session = req.cookies.session;
+        }
+
+        if (session) {
+            logger.debug('Session found! User email:', session[0]);
+        } else {
+            // Attempt to create a synthetic but functional session object?
+            session = [
+                ''.concat("anonymous@",clientIp),
+                "anonymous",
+                "access_token",
+                "false"
+            ];
+            // spoof the request to include the new session cookie as well?
+            req.cookies.session = res.cookie("session", session, {
+                domain: req.hostname //require explicit domain set to work with subdomains
+            });
+        }
+        logger.debug('Session: ', session);
+
+        next();
+    };
+}
+
 //start function
 function start(config) {
     // setup logging
@@ -89,16 +130,8 @@ function start(config) {
     // express plugins
     app.use(cookieParser());
 
-    // session management
-    app.use('/', function(req, res, next) {
-        let fullClientIp = requestIp.getClientIp(req);
-        let clientIpPart = fullClientIp.split(':');
-        let clientIp = clientIpPart[clientIpPart.length - 1]
-        logger.debug('Client IP:', clientIp);
-        logger.debug("Request Headers:\n",JSON.stringify(req.headers));
-        logger.debug('Request Type:', req.method);
-        next();
-    });
+    // Elaborated session management
+    app.use(getOrCreateSession());
 
     /*
      Will only insert middleware to process Google Analytics if a non-empty
