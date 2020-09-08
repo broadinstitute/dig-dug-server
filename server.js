aed9fec38d3b03f1a19ec8c2686d9aa6cdcf14f2
Quy <quy@broad.dev>
2020-09-08 17:45:41 -0400

    no longer connect to logins db at startup

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-const ExpressGA = require("express-universal-analytics")
+const ExpressGA = require("express-universal-analytics");
 const express = require("express");
-const express_session = require('express-session')
+const express_session = require("express-session");
 
-const getRepoInfo = require('git-repo-info');
+const getRepoInfo = require("git-repo-info");
 
 const cookieParser = require("cookie-parser");
 const path = require("path");
 const log4js = require("log4js");
 const google = require("./google");
 const logins = require("./logins");
-
 
 let logger = undefined;
 
@@ -43,11 +42,13 @@ function enable_logging(config) {
 }
 
 function create_routes(config, app) {
-    const { git_portal_version, git_server_version } = gitConfig(config.content.dist);
+    const { git_portal_version, git_server_version } = gitConfig(
+        config.content.dist
+    );
     const git_application_versions = [
         git_portal_version,
         git_server_version
-    ].join(';')
+    ].join(";");
 
     // Google OAuth
     app.get("/login", google.logInLink);
@@ -66,7 +67,6 @@ function logOut(req, res) {
     res.redirect("/");
 }
 
-
 //get domain from hostname
 function getDomain(host) {
     let parts = host.split(".");
@@ -74,7 +74,6 @@ function getDomain(host) {
         return parts[parts.length - 2] + "." + parts[parts.length - 1];
     } else return host;
 }
-
 
 /**
  * Event Log web service endpoint for Google Analytics reporting.
@@ -89,24 +88,25 @@ function getDomain(host) {
  */
 function eventLog(git_application_version) {
     return (req, res) => {
-        req.visitor.setUid(logins.getUserId(req))
+        req.visitor.setUid(logins.getUserId(req));
         // custom dimensions
         // see the analytics account for their descriptions
         // !!! NOTE: you will have to define these if you migrate analytics accounts! !!!
         // req.visitor.set("cd1", git_portal_version)        // gitPortalVersion in Analytics
         // req.visitor.set("cd2", git_server_version)        // gitServerVersion in Analytics
 
-        req.visitor.event({
-            ec: req.query.category,
-            ea: req.query.action,
-            el: req.query.label + ';' + git_application_version,
-            ev: 0,
-            dp: req.query.page,
-        }).send();
-        res.send('ok');
-    }
+        req.visitor
+            .event({
+                ec: req.query.category,
+                ea: req.query.action,
+                el: req.query.label + ";" + git_application_version,
+                ev: 0,
+                dp: req.query.page
+            })
+            .send();
+        res.send("ok");
+    };
 }
-
 
 function validateConfig(config) {
     let valid = true;
@@ -119,20 +119,27 @@ function validateConfig(config) {
     return valid;
 }
 
-
 function gitConfig(dist) {
     const git_server_info = getRepoInfo(__dirname);
     const git_portal_info = getRepoInfo(path.dirname(dist));
-    const git_portal_version = ['portal', git_portal_info.branch, git_portal_info.sha.substring(0, 7)].join(':');
-    const git_server_version = ['server', git_server_info.branch, git_server_info.sha.substring(0, 7)].join(':');
+    const git_portal_version = [
+        "portal",
+        git_portal_info.branch,
+        git_portal_info.sha.substring(0, 7)
+    ].join(":");
+    const git_server_version = [
+        "server",
+        git_server_info.branch,
+        git_server_info.sha.substring(0, 7)
+    ].join(":");
     return {
-        git_portal_version, git_server_version
-    }
+        git_portal_version,
+        git_server_version
+    };
 }
 
 //start function
 function start(config) {
-
     // setup logging
     enable_logging(config);
 
@@ -149,22 +156,19 @@ function start(config) {
     // RMB: moved the database connection method up here
     // in advance of the express_session initialisation?
     // TODO: modify the logins database to apply it to the express-session
-    logins.connectToDatabase(config);
+    //logins.connectToDatabase(config);
 
     // Deploy the Express session middleware
     // TODO: need review on how it is being used, and perhaps adapt to Portal session management(?)
     app.use(
-        express_session(
-            {
-                secret: config.session.secret || 'dummy-session-secret',
-                resave: false,
-                saveUninitialized: false,
-                cookie: {
-                    maxAge: config.session.cookie.maxAge,
-
-
-                },
-            })
+        express_session({
+            secret: config.session.secret || "dummy-session-secret",
+            resave: false,
+            saveUninitialized: false,
+            cookie: {
+                maxAge: config.session.cookie.maxAge
+            }
+        })
     );
 
     // express plugins
@@ -181,16 +185,13 @@ function start(config) {
     let ua_id = config.auth.google.UAId;
     if (ua_id) {
         app.use(
-            ExpressGA(
-                {
-                    uaCode: ua_id,
-                    // default GA cookie '_ga' assumed
-                    // extract user id from request
-                    reqToUserId: logins.getUserId,
-                    autoTrackPages: false,
-                }
-
-            )
+            ExpressGA({
+                uaCode: ua_id,
+                // default GA cookie '_ga' assumed
+                // extract user id from request
+                reqToUserId: logins.getUserId,
+                autoTrackPages: false
+            })
         );
     }
 
